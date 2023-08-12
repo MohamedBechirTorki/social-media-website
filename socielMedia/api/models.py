ba6ddb2e85de6648c310f4eff498f0473b631fc0
Mohamed Bechir Torki <mohamedbechirtorki@gmail.com>
2023-08-12 20:30:29 +0100

    m

--- socielMedia/api/models.py
+++ socielMedia/api/models.py
@@ -1,4 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model) :
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(null=True, blank=True)
+    birth_date = models.DateTimeField()
+    
+    
+    def __str__(self) :
+        return self.user.username[:50]
+
+class Post(models.Model) :
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+
+    def __str__(self) :
+        return "Post: " + self.id + " by " + self.poster.username[:10]
 
